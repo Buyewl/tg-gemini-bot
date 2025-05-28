495e778cb4fd9800ef384b27b0a625412f71f97f
Buyewl <129209691+Buyewl@users.noreply.github.com>
2025-05-29 05:44:43 +0700

    Update handle.py

--- api/handle.py
+++ api/handle.py
@@ -36,12 +36,14 @@ def handle_message(update_data):
     #    strupdate_data = str(update_data)
     #    start = strupdate_data.find("\'id\': ")
     #    from_id = strupdate_data[start + 6: start + 16]
-    #    send_message( from_id,
-    f"You have sent an unknown event. "
+    #    send_message(
+    from_id,
+    f"You have sent an unknown event. "  # Memulai string di baris baru
     f"Please send the following information to the bot administrator.\n"
     f"您发送了一个未知事件，请把下面信息发送至bot管理员。\n\n"
-    f"{update_data}\n"
-    f"{e}" )    
+    f"{update_data}\n"  # Placeholder f-string di baris terpisah
+    f"{e}"              # Placeholder f-string lainnya
+)    
     #    send_message(admin_id, f"收到了一个未知事件，原文为：\n{update_data}\n错误为：\n{e}")
 
     update = Update(update_data)
