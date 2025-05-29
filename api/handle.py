0ac2d7c231dd788eecfbd3599c8bb07e59689cdf
Buyewl <129209691+Buyewl@users.noreply.github.com>
2025-05-29 08:02:50 +0700

    Update handle.py

--- api/handle.py
+++ api/handle.py
@@ -62,15 +62,22 @@ def handle_message(update_data):
         if response_text != "":
             send_message(update.chat_id, response_text)
             if update.is_group:
-                log = f"@{update.user_name} id:`{update.from_id}` {group} @{update.group_name} id:`{update.chat_id}`{the_content_sent_is}\n{update.text}\n{the_reply_content_is}\n{response_text}"
+                log = (
+                    f"@{update.user_name} id:`{update.from_id}` {group} "
+                    f"@{update.group_name} id:`{update.chat_id}`"
+                    f"{the_content_sent_is}\n"
+                    f"{update.text}\n"
+                    f"{the_reply_content_is}\n"
+                    f"{response_text}")
             else:
                 log = f"@{update.user_name} id:`{update.from_id}`{the_content_sent_is}\n{update.text}\n{the_reply_content_is}\n{response_text}"
             send_log(log)
 
     elif not authorized:
         if update.is_group:
-            send_message(update.chat_id, f"{group_no_permission_info}\nID:`{update.chat_id}`")
-            log = f"@{update.user_name} id:`{update.from_id}` {group} @{update.group_name} id:`{update.chat_id}`{no_rights_to_use},{the_content_sent_is}\n{update.text}"
+            send_message(
+                update.chat_id,
+                f"{group_no_permission_info}\nID:`{update.chat_id}`")
         else:
             send_message(
             update.from_id, f"{user_no_permission_info}\nID:`{update.from_id}`")
@@ -86,16 +93,24 @@ def handle_message(update_data):
         chat = chat_manager.get_chat(history_id)
         anwser = chat.send_message(update.text)
         extra_text = (
-            f"\n\n{prompt_new_info}" if chat.history_length >= prompt_new_threshold*2 else ""
-        )
+            f"\n\n{prompt_new_info}" if chat.history_length >= prompt_new_threshold*2 else "")
         response_text = f"{anwser}{extra_text}"
         send_message(update.chat_id, response_text)
         dialogueLogarithm = int(chat.history_length/2)
         if update.is_group:
-            log = f"@{update.user_name} id:`{update.from_id}` {group} @{update.group_name} id:`{update.chat_id}`{the_content_sent_is}\n{update.text}\n{the_reply_content_is}\n{response_text}\n{the_logarithm_of_historical_conversations_is}{dialogueLogarithm}"
+            log = (
+                f"@{update.user_name} id:`{update.from_id}` {group} "
+                f"@{update.group_name} id:`{update.chat_id}`"
+                f"{the_content_sent_is}\n{update.text}\n"
+                f"{the_reply_content_is}\n{response_text}\n"
+                f"{the_logarithm_of_historical_conversations_is}{dialogueLogarithm}")
         else:
-            log = f"@{update.user_name} id:`{update.from_id}`{the_content_sent_is}\n{update.text}\n{the_reply_content_is}\n{response_text}\n{the_logarithm_of_historical_conversations_is}{dialogueLogarithm}"
-        send_log(log)
+            log = (
+                f"@{update.user_name} id:`{update.from_id}`"
+                f"{the_content_sent_is}\n{update.text}\n"
+                f"{the_reply_content_is}\n{response_text}\n"
+                f"{the_logarithm_of_historical_conversations_is}{dialogueLogarithm}")
+            send_log(log)
 
     elif update.type == "photo":
         chat = ImageChatManger(update.photo_caption, update.file_id)
@@ -104,18 +119,24 @@ def handle_message(update_data):
         # Use the reply_to_message_id parameter to let the bot reply to
         # a specific image.
         send_message(
-            update.chat_id, response_text, reply_to_message_id=update.message_id
-        )
+            update.chat_id, response_text, reply_to_message_id=update.message_id)
 
         photo_url = chat.tel_photo_url()
         imageID = update.file_id
         if update.is_group:
-            log = f"@{update.user_name} id:`{update.from_id}` {group} @{update.group_name} id:`{update.chat_id}`[photo]({photo_url}),{the_accompanying_message_is}\n{update.photo_caption}\n{the_reply_content_is}\n{response_text}"
+            log = (
+                f"@{update.user_name} id:`{update.from_id}` {group} "
+                f"@{update.group_name} id:`{update.chat_id}`[photo]({photo_url}),"
+                f"{the_accompanying_message_is}\n{update.photo_caption}\n"
+                f"{the_reply_content_is}\n{response_text}")
         else:
-            log = f"@{update.user_name} id:`{update.from_id}`[photo]({photo_url}),{the_accompanying_message_is}\n{update.photo_caption}\n{the_reply_content_is}\n{response_text}"
-        send_image_log("", imageID)
-        send_log(log)
-
+            log = (
+                f"@{update.user_name} id:`{update.from_id}`[photo]({photo_url}),"
+                f"{the_accompanying_message_is}\n{update.photo_caption}\n"
+                f"{the_reply_content_is}\n{response_text}")
+            send_image_log("", imageID)
+            send_log(log)
+        
     else:
         send_message(
             update.chat_id, f"{unable_to_recognize_content_sent}\n\n/help")
